1cb36991e7929501b50c4b4ed0a3cc5d2f4ad41f
Yana Glad <55493845+YanaGlad@users.noreply.github.com>
2021-08-17 14:05:51 +0300

    Добавила фрагменты и необходимые зависимости

---
+++ app/src/main/java/com/example/gladkikhvlasovtinkoff/WelcomeFragment.kt
@@ -0,0 +1,35 @@
+package com.example.gladkikhvlasovtinkoff
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.gladkikhvlasovtinkoff.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentWelcomeBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
