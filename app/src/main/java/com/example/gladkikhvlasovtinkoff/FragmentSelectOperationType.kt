5dabaf5aa2ca3823ad7986654eefa7ebd9239dcd
Yana Glad <55493845+YanaGlad@users.noreply.github.com>
2021-08-19 00:03:55 +0300

    Check отображается при нажатии на доход/расход

--- app/src/main/java/com/example/gladkikhvlasovtinkoff/FragmentSelectOperationType.kt
+++ app/src/main/java/com/example/gladkikhvlasovtinkoff/FragmentSelectOperationType.kt
@@ -9,7 +9,7 @@ import androidx.navigation.fragment.findNavController
 import com.example.gladkikhvlasovtinkoff.databinding.FragmentConfirmOperationCreatedBinding
 import com.example.gladkikhvlasovtinkoff.databinding.FragmentSelectOperationTypeBinding
 
-class FragmentSelectOperationType : Fragment(){
+class FragmentSelectOperationType : Fragment() {
 
     private var _binding: FragmentSelectOperationTypeBinding? = null
     private val binding get() = _binding!!
@@ -21,8 +21,24 @@ class FragmentSelectOperationType : Fragment(){
     ): View {
         _binding = FragmentSelectOperationTypeBinding.inflate(inflater)
 
+        binding.textCostsType.setOnClickListener {
+            if (binding.checkCostsOperationType.visibility == View.GONE) {
+                binding.checkCostsOperationType.visibility = View.VISIBLE
+                binding.checkIncomeOperationType.visibility = View.GONE
+            } else binding.checkCostsOperationType.visibility = View.GONE
+        }
+
+        binding.textIncomeType.setOnClickListener {
+            if (binding.checkIncomeOperationType.visibility == View.GONE) {
+                binding.checkIncomeOperationType.visibility = View.VISIBLE
+                binding.checkCostsOperationType.visibility = View.GONE
+            } else binding.checkIncomeOperationType.visibility = View.GONE
+        }
+
+
         binding.buttonConfirmOperationType.setOnClickListener {
-            val action = FragmentSelectOperationTypeDirections.actionFragmentSelectOperationTypeToFragmentSelectOperationCategory()
+            val action =
+                FragmentSelectOperationTypeDirections.actionFragmentSelectOperationTypeToFragmentSelectOperationCategory()
             findNavController().navigate(action)
         }
 
