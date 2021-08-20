49235c3c8540c797fd86ca94af87ac6e6400eb4f
Giksengik <giksengik@gmail.com>
2021-08-20 22:36:22 +0300

    Create extension for styling

--- app/src/main/java/com/example/gladkikhvlasovtinkoff/extension/StringExtension.kt
+++ app/src/main/java/com/example/gladkikhvlasovtinkoff/extension/StringExtension.kt
@@ -1,12 +1,38 @@
 package com.example.gladkikhvlasovtinkoff.extension
 
-fun String.styleText() : String {
-    var result = ""
-    for (i in this.indices.reversed()) {
-        result += this[i]
-        if ((i - this.length) % 3 == 0) {
-            result+=" "
+fun String.convertToStyled() : String {
+    val firstIndex = this.indexOfFirst{ char ->
+        char == ','
+    }
+    val firstComma = if(firstIndex != -1) firstIndex
+    else this.length - 1
+    var digitInLastGroup = 0
+    return buildString {
+        for(i in firstComma - 1 downTo 0 ) {
+            if (digitInLastGroup < 3) {
+                digitInLastGroup++
+                append(this@convertToStyled[i])
+            }else{
+                digitInLastGroup = 1
+                append(" ")
+                append(this@convertToStyled[i])
+            }
+        }
+    }.reversed() + "," + this.substring(firstComma + 1)
+}
+
+fun String.convertFromStyled() : String =
+    buildString {
+        var containDot = false
+        for(char in this@convertFromStyled){
+            if(char != ' ') {
+                if(char == '.' || char == ',' && !containDot) {
+                    containDot = true
+                    append(',')
+                }
+                else {
+                    append(char)
+                }
+            }
         }
     }
-    return result.reversed() + " ₽"
-}
