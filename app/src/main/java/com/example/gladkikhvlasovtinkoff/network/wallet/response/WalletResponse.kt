ec0403d2c9c073465d251bd4f74aa0852076f856
Giksengik <giksengik@gmail.com>
2021-08-21 16:28:06 +0300

    Add and refactor network entities

--- app/src/main/java/com/example/gladkikhvlasovtinkoff/network/wallet/response/WalletResponse.kt
+++ app/src/main/java/com/example/gladkikhvlasovtinkoff/network/wallet/response/WalletResponse.kt
@@ -3,7 +3,7 @@ package com.example.gladkikhvlasovtinkoff.network.wallet.response
 import com.google.gson.annotations.SerializedName
 
 class WalletResponse (
-        @SerializedName("balance") val balance : Long,
+        @SerializedName("balance") val balance : String,
         @SerializedName("currency") val currency : CurrencyResponse,
         @SerializedName("id") val id : Long,
         @SerializedName("name") val name : String,
