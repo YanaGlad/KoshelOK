c2125b1c84a76a1790c0113d59042626b7487cbd
Yana Glad <55493845+YanaGlad@users.noreply.github.com>
2021-08-23 10:16:51 +0300

    Currency motion layout in now matching parent size

--- app/src/main/java/com/example/gladkikhvlasovtinkoff/ui/ui/wallets/WalletsFragment.kt
+++ app/src/main/java/com/example/gladkikhvlasovtinkoff/ui/ui/wallets/WalletsFragment.kt
@@ -23,7 +23,6 @@ class WalletsFragment : ToolbarFragment() {
 
     private var operationsAdapter: WalletsAdapter? = null
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
