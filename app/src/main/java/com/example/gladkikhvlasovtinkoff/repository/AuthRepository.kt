a0e1f2a50e6eb2032af283e4ab5fef8aba18c5b2
Giksengik <giksengik@gmail.com>
2021-08-26 07:53:44 +0300

    Fix and setup auth

--- app/src/main/java/com/example/gladkikhvlasovtinkoff/repository/AuthRepository.kt
+++ app/src/main/java/com/example/gladkikhvlasovtinkoff/repository/AuthRepository.kt
@@ -1,8 +1,9 @@
 package com.example.gladkikhvlasovtinkoff.repository
 
+import com.example.gladkikhvlasovtinkoff.ui.ui.welcome.AuthViewState
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import io.reactivex.Completable
+import io.reactivex.Single
 
 interface AuthRepository {
-    fun logInWithAccount(account : GoogleSignInAccount) : Completable
+    fun logInWithAccount(account : GoogleSignInAccount) : Single<AuthViewState>
 }
